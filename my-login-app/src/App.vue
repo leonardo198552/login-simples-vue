aae4791b4b2a0fb9c01c43ec3fc890cdb8deb90b
Leonardo Junio Andrade <leoandrade1012@hotmail.com>
2023-03-23 10:00:43 -0300

    Login Simples

---
+++ my-login-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <login-form></login-form>
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+
+export default {
+  components: {
+    LoginForm,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
---
+++ my-login-app/src/components/LoginForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="login-form">
+    <h1>Login</h1>
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      // Aqui você pode adicionar sua lógica de autenticação, como chamar uma API
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login-form {
+  width: 300px;
+  margin: 0 auto;
+}
+
+.login-form h1 {
+  text-align: center;
+}
+
+.login-form div {
+  margin-bottom: 1em;
+}
+
+.login-form label {
+  display: block;
+}
+</style>
